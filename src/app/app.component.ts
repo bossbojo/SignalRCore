9050fcc06030eab8a2632ef9c4691433475d23c8
paramat <paramat.si.56@ubu.ac.th>
2018-10-06 17:35:46 +0700

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr';
+import * as signalR from '@aspnet/signalr';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private hubConnection: HubConnection;
+  UserId = 'XX012';
+  Chanel = 'YourChanel';
+  constructor() {
+  }
+  ngOnInit() {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('http://localhost:5000/notification')
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    this.hubConnection.start().then(() => {
+
+      console.log('Connection started!');
+
+      this.hubConnection
+        .invoke('OnConnectHub', this.UserId, this.Chanel)
+        .catch(err => console.error(err));
+
+    }).catch(err => console.log('Error while establishing connection :('));
+
+    this.hubConnection.on('OnConnected', (Ojson: string) => {
+      if (Ojson) {
+        console.log(JSON.parse(Ojson));
+      }
+    });
+    this.hubConnection.on('ReceiveNotification', (Ojson: string) => {
+      if (Ojson) {
+        console.log(JSON.parse(Ojson));
+      }
+    });
+
+  }
 }
